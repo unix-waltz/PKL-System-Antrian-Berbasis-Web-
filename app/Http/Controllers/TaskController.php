d5d0867e6f65ebba2bc0c4b167126e0a7dc5ea02
unix-waltz <riosraskaa@gmail.com>
2023-09-04 17:42:00 +0700

    m

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function pendaftaran(){
+return view('feature.pendaftaran');
+    }
+      public function jadwal(){
+        
+    }
+      public function penugasan(){
+        
+    }
+      public function kemajuan(){
+        
+    }
+      public function dokumen(){
+        
+    }
+      public function komunikasi(){
+        
+    }
+      public function evaluasi(){
+        
+    }
+      public function pelaporan(){
+        
+    }
+      public function keamanan(){
+        
+    }
+      public function dashboard(){
+        
+    }
+      public function pengingat(){
+        
+    }
+      public function integrasi(){
+        
+    }
+      public function history(){
+        
+    }
+      public function pengarsipan(){
+        
+    }
+    public function pengelolaan(){
+
+    }
+
+}
--- resources/views/component/nav.blade.php
+++ resources/views/component/nav.blade.php
@@ -10,10 +10,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active " aria-current="page" href="#">Home</a>
+                    <a class="nav-link" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">About</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -26,9 +26,7 @@
                         <li><a class="dropdown-item" href="#">Something else here</a></li>
                     </ul>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link">Disabled</a>
-                </li>
+              
             </ul>
             <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
