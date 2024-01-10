13f8e0df6e66bbeb3a3d94a7911cc7b7fae5c838
unix-waltz <riosraskaa@gmail.com>
2024-01-10 13:29:48 +0700

    h

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -7,13 +7,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
   
-<link rel="stylesheet" href="{{secure_asset('css/style.css')}}">
+<link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
   <body>
     @include('component.nav')
     @yield('content')
     @include('component.footer')
-    <script src="{{secure_asset('js/script.js')}}"></script>
+    <script src="{{asset('js/script.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   </body>
 </html>
